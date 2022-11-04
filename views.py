e0142f1a9b2c321c32dbbadf924cfc2c6b3b7823
yehans41 <116742021+yehans41@users.noreply.github.com>
2022-11-04 09:41:06 -0500

    Add files via upload

---
+++ views.py
@@ -0,0 +1,22 @@
+
+from flask import Blueprint, render_template
+
+
+views = Blueprint(__name__, "views")
+
+@views.route("/")
+def home():
+    return render_template("home.html")
+
+
+@views.route("/profile/<username>")
+def profile(username):
+    return render_template("home.html", name=username)
+
+@views.route("/templates/fbla.html")
+def fbla():
+    return render_template("fbla.html")
+
+@views.route("/templates/nhs.html")
+def nhs():
+    return render_template("nhs.html")
