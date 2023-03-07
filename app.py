200c99e03a8366e3de027cab50027f37d5918961
yehans41 <yehans5178@gmail.com>
2023-03-07 09:19:58 -0600

    search + db

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Blueprint, render_template
 from flask import Flask,redirect,url_for,render_template,request, jsonify
 
 app = Flask(__name__)
-
+#database connection
 def get_db_connection():
     conn = sqlite3.connect('RealDB.db')
     conn.row_factory = sqlite3.Row #goes through database
@@ -15,14 +15,18 @@ def get_db_connection():
 @app.route("/")
 def home():
     conn = get_db_connection()
-    clubs = conn.execute('SELECT * from Clubs').fetchall()
+    #clubs = conn.execute('SELECT * from clubs LEFT JOIN weekdays on clubs.name = weekdays.name').fetchall()
+    clubs = conn.execute('SELECT * from clubs').fetchall()
+    #weekdays = conn.execute('SELECT * from Weekdays').fetchall()
     conn.close()
     return render_template("home.html", clubs=clubs)
 @app.route("/profile/<username>")
 @app.route("/templates/base.html")
 def base():
     conn = get_db_connection()
-    clubs = conn.execute('SELECT * from Clubs').fetchall()
+    #clubs = conn.execute('SELECT * from clubs LEFT JOIN weekdays on clubs.name = weekdays.name').fetchall()
+    clubs = conn.execute('SELECT * from clubs').fetchall()
+    #weekdays = conn.execute('SELECT * from Weekdays').fetchall()
     conn.close()
     Name = request.args.get('Name')
     return render_template("base.html", clubs=clubs, Name=Name)
--- config.py
+++
@@ -1,6 +0,0 @@
-#database creation
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'my_db.db')
