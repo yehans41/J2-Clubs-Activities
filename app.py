7a5576a96e61bdb04e9b0f6c395e049295d04e36
yehans41 <yehans5178@gmail.com>
2023-03-07 10:04:24 -0600

    td

--- app.py
+++ app.py
@@ -15,8 +15,8 @@ def get_db_connection():
 @app.route("/")
 def home():
     conn = get_db_connection()
-    #clubs = conn.execute('SELECT * from clubs LEFT JOIN weekdays on clubs.name = weekdays.name').fetchall()
-    clubs = conn.execute('SELECT * from clubs').fetchall()
+    clubs = conn.execute('SELECT * from clubs LEFT JOIN weekdays on clubs.name = weekdays.name').fetchall()
+    #clubs = conn.execute('SELECT * from clubs').fetchall()
     #weekdays = conn.execute('SELECT * from Weekdays').fetchall()
     conn.close()
     return render_template("home.html", clubs=clubs)
@@ -24,8 +24,8 @@ def home():
 @app.route("/templates/base.html")
 def base():
     conn = get_db_connection()
-    #clubs = conn.execute('SELECT * from clubs LEFT JOIN weekdays on clubs.name = weekdays.name').fetchall()
-    clubs = conn.execute('SELECT * from clubs').fetchall()
+    clubs = conn.execute('SELECT * from clubs LEFT JOIN weekdays on clubs.name = weekdays.name').fetchall()
+    #clubs = conn.execute('SELECT * from clubs').fetchall()
     #weekdays = conn.execute('SELECT * from Weekdays').fetchall()
     conn.close()
     Name = request.args.get('Name')
